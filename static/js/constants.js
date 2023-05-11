76cf097d5b3f6bc14e057b8fa5d23e93f0c7784f
Artrois <paul.wulf@web.de>
2023-05-11 21:36:48 +0200

    changed Sara to Alice. Modified instructions

--- static/js/constants.js
+++ static/js/constants.js
@@ -2,7 +2,7 @@
 //action to be triggered/sent to rasa upon connection. Leave blank if you dont want to trigger any action
 const action_name = "";
 //message to be sent to rasa upon connection. This is to pass an initial message to rasa to make aware of new client.
-const custom_hello_message_to_rasa = '/restart; /vaig_event_start{"caller": "+49000000", "callee": "+4923199768543", "callerHost": "None", "callerDisplayName": "None", "vaigConversationId": "None", "calleeHost": "None"}';
+const custom_hello_message_to_rasa = '/restart; /vaig_event_start{"caller": "", "callee": "+4923199768543", "callerHost": "None", "callerDisplayName": "None", "vaigConversationId": "None", "calleeHost": "None"}';
 //this one you need to adjust if you run your bot on a server/cloud and not locally
 const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
 //if set the agent will be opened once document loaded. Used in script.js
